6020823ba23e47ca55b5ae1aac917147006d6f8c
Nicolas Gagnon <2163598@cegepmontpetit.ca>
2024-02-12 14:58:27 -0500

    Polling

---
+++ PollingEtSignalR/labo.signalr.api/MatchHub.cs
@@ -0,0 +1,44 @@
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+
+public class MatchHub:Hub
+{
+    private readonly ApplicationDbContext _context;
+
+    public MatchHub(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task EnvoyerAUnUsager(int value, string userId)
+    {
+        await Clients.User(userId).SendAsync("tasklist", value);
+    }
+
+    public async Task AddTask(int value, string taskText)
+    {
+        UselessTask uselessTask = new UselessTask()
+        {
+            Completed = false,
+            Text = taskText
+        };
+        _context.UselessTasks.Add(uselessTask);
+        await _context.SaveChangesAsync();
+
+        await Clients.All.SendAsync("tasklist", value);
+    }
+
+    public async Task CompleteTask(int value, int id)
+    {
+        UselessTask? task = await _context.FindAsync<UselessTask>(id);
+        if (task != null)
+        {
+            task.Completed = true;
+            await _context.SaveChangesAsync();
+            await Clients.All.SendAsync("tasklist", value);
+        }
+
+        
+    }
+}
--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UselessTask } from '../models/UselessTask';
+import {HttpClient} from "@angular/common/http";
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-polling',
@@ -15,17 +17,24 @@ export class PollingComponent implements OnInit {
   ngOnInit(): void {
     this.updateTasks();
   }
+  constructor(public http: HttpClient){}
 
-  complete(id: number) {
+  async complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    let result = await lastValueFrom(this.http.get<any>('http://localhost:5042/api/UselessTasks/Complete/'+id));
+    console.log(result)
   }
 
-  addtask() {
+  async addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    let result = await lastValueFrom(this.http.post<any>('http://localhost:5042/api/UselessTasks/Add?taskText='+this.taskname,null));
+    console.log(result)
   }
 
-  updateTasks() {
+  async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    this.tasks = await lastValueFrom(this.http.get<any>('http://localhost:5042/api/UselessTasks/GetAll'));
+    setTimeout(()=> {this.updateTasks()},500)
   }
 }
